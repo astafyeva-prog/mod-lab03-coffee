fc544231e1f095f08770c3cf10c0f9061267b028
astafyeva-prog <astafeva0404@gmail.com>
2026-05-17 22:46:52 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -1,48 +1,39 @@
-#ifndef AUTOMATA_H
-#define AUTOMATA_H
+// Copyright 2026 Anastasia Astafeva
 
-#include <iostream>
-#include <vector>
+#ifndef INCLUDE_AUTOMATA_H_
+#define INCLUDE_AUTOMATA_H_
+
 #include <string>
-
-using namespace std;
+#include <vector>
 
 enum STATES {
-    OFF,
-    WAIT,
-    ACCEPT,
-    CHECK,
-    COOK
+  OFF,
+  WAIT,
+  ACCEPT,
+  CHECK,
+  COOK
 };
 
 class Automata {
-private:
-    int cash;
-    vector<string> menu;
-    vector<int> prices;
-    STATES state;
+ public:
+  Automata();
 
-public:
-    Automata();
+  void on();
+  void off();
+  void coin(int value);
+  void getMenu();
+  STATES getState();
+  void choice(int index);
+  bool check(int index);
+  void cancel();
+  void cook(std::string drink);
+  void finish();
 
-    void on();
-    void off();
-
-    void coin(int value);
-
-    void getMenu();
-
-    STATES getState();
-
-    void choice(int index);
-
-    bool check(int index);
-
-    void cancel();
-
-    void cook(string drink);
-
-    void finish();
+ private:
+  int cash;
+  std::vector<std::string> menu;
+  std::vector<int> prices;
+  STATES state;
 };
 
-#endif
+#endif  // INCLUDE_AUTOMATA_H_
