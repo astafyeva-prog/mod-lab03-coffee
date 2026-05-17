b118e08f1006dd72a77f0fb4662609abd0917314
astafyeva-prog <astafeva0404@gmail.com>
2026-05-17 21:39:29 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,29 @@
-// Copyright 2022 GHA Test Team
+#include "../include/Automata.h"
+#include <cassert>
 
-#include <gtest/gtest.h>
-#include "Automata.h"
+int main() {
+
+    Automata a;
+
+    a.on();
+    assert(a.getState() == WAIT);
+
+    a.coin(50);
+    assert(a.getState() == ACCEPT);
+
+    assert(a.check(0) == true);
+
+    a.choice(0);
+    assert(a.getState() == WAIT);
+
+    a.coin(20);
+    assert(a.check(1) == false);
+
+    a.cancel();
+    assert(a.getState() == WAIT);
+
+    a.off();
+    assert(a.getState() == OFF);
+
+    return 0;
+}
