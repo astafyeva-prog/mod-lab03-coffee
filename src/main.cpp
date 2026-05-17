b82f12374f05f221a7fb280ddc8c3eadfea274f3
astafyeva-prog <astafeva0404@gmail.com>
2026-05-17 22:55:51 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,35 @@
-// Copyright 2026 Anastasia Astafeva
+
+// Copyright 2026 Astafeva Anastasiia
+
 #include "Automata.h"
 
 int main() {
-
-    Automata a;
-
-    a.on();
-
-    a.getMenu();
-
-    a.coin(100);
-
-    a.choice(1);
-
-    a.off();
-
+    std::vector <std::string> states =
+    { "OFF", "WAIT", "ACCEPT", "CHECK", "COOK" };
+    Automata cof;
+    std::cout << states[cof.getState()] << std::endl;
+    std::cout << "Switching on" << std::endl;
+    cof.on();
+    std::cout << states[cof.getState()] << std::endl;
+    std::cout << "What is on the menu" << std::endl;
+    std::vector <std::string> menu = cof.getMenu();
+    for (std::string s : menu) {
+        std::cout << s << std::endl;
+    }
+    std::cout << "I want coffee. Let's add some coins" << std::endl;
+    cof.coin();
+    std::cout << states[cof.getState()] << std::endl;
+    cof.coin();
+    std::cout << states[cof.getState()] << std::endl;
+    std::cout << "Hehe, let's try to buy"
+        "coffee with only 2 coins." << std::endl;
+    cof.choice("Coffee");
+    std::cout << states[cof.getState()] << std::endl;
+    std::cout << "Hmmm, nothing. Okay then. I'll buy tea" << std::endl;
+    cof.choice("Tea");
+    std::cout << states[cof.getState()] << std::endl;
+    std::cout << "Yummy. Let's turn it off" << std::endl;
+    cof.off();
+    std::cout << states[cof.getState()] << std::endl;
     return 0;
 }
